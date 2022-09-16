4790a4e53a41e6c937a8a9f001214ccef7cef1eb
Destinee Saunders <dsaunders290@gmail.com>
2022-09-15 23:25:20 -0400

    commit tree bde25a8f5f8a670d7e1b6471d9c0bc6d8d53cacb

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, name){
+const matching = drivers.filter(driver => name.toLowerCase() === driver.toLowerCase())
+return matching
+} 
+function fuzzyMatch(drivers, name){
+    const matching = drivers.filter(driver => driver.startsWith(name))
+    return matching
+}
+function matchName(drivers, name){
+const matching = drivers.filter(driver => driver.name === name)
+return matching
+}
